aba6d693b217a10a084e93c1ecd58119038098a5
Ivan Almada <ivan.almada@me.com>
2023-10-19 11:57:26 +0200

    Initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ivan Almada on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
